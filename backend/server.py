7b46f0b18bd534abdd41afb880a07c5070a84d25
[QA3]양정은 <did00417@naver.com>
2026-01-08 15:57:52 +0900

    python gui 흐름 테스트

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,38 @@
 # 가장 심플한 파이썬 API 서버 (Flask)
 # 실행하면: http://localhost:5000/ 로 접속 시 "Hello, World!" 반환
 
-from flask import Flask
+from flask import Flask , jsonify, request
 
 app = Flask(__name__)
 
 @app.get("/")
 def hello():
     return "Hello, World!"
+
+@app.get("/ping")
+def ping():
+    return jsonify({"status": "test successful"})
+
+@app.get("/users")
+def get_users():
+    return jsonify([
+        {"id": 1, "username": "test1"},
+        {"id": 2, "username": "test2"},
+    ])
+
+@app.get("/users/<int:user_id>")
+def get_user(user_id):
+    if user_id == 1:
+        return jsonify({"id": 1, "username": "test1"})
+    return jsonify({"message": "유저 없음"}), 404
+
+@app.post("/users")
+def create_user():
+    data = request.json
+    return jsonify({
+        "message": "회원가입 완료",
+        "username": data.get("username")
+    }), 201
 
 if __name__ == "__main__":
     # host=0.0.0.0 : 외부(다른 PC/컨테이너)에서도 접근 가능하게 열고 싶을 때
